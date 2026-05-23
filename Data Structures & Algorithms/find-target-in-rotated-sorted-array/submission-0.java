03708d2a6344470d7615f0c35a07974b70b9992b
Idole Koun <idole.koun@outlook.com>
2026-05-22 23:04:53 -0400

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // Find the index of the smallest element (or largest depending on the implementation)
+        int n = nums.length;
+        int lo = 0;
+        int hi = n - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) >> 1;
+            if (nums[mid] > nums[n - 1]) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        // Search the left half
+        int ans = binarySearch(0, lo - 1, target, nums);
+        if (ans > -1) {
+            return ans;
+        }
+
+        // Search right half
+        return binarySearch(lo, n - 1, target, nums);
+    }
+
+    private int binarySearch(int lo, int hi, int target, int[] nums) {
+        while (lo <= hi) {
+            int mid = (lo + hi) >> 1;
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            } else if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
