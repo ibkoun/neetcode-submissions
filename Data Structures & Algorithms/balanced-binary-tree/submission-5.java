b9e620fa359cf6fe3d315bd8399cb2f0467d0f82
Idole Koun <idole.koun@outlook.com>
2026-09-07 22:14:30 -0400

    Add: balanced-binary-tree - submission-5

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-5.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        if (!isBalanced(root.left)) {
+            return false;
+        }
+
+        if (!isBalanced(root.right)) {
+            return false;
+        }
+
+        var left = DFS(root.left);
+        var right = DFS(root.right);
+
+        return Math.abs(left - right) <= 1;
+    }
+
+    private int DFS(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        var left = DFS(node.left);
+        var right = DFS(node.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
