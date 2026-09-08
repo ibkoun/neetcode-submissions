e318da696a0add6de851a70d09445678f3c666fa
Idole Koun <idole.koun@outlook.com>
2026-09-08 01:17:45 -0400

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        }
+
+        var queue1 = new ArrayDeque<TreeNode>();
+        var queue2 = new ArrayDeque<TreeNode>();
+        queue1.add(p);
+        queue2.add(q);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            var node1 = queue1.removeFirst();
+            var node2 = queue2.removeFirst();
+            if ((node1.val != node2.val) || (node1.left != null && node2.left == null)
+                || (node1.left == null && node2.left != null)
+                || (node1.right != null && node2.right == null)
+                || (node1.right == null && node2.right != null)) {
+                return false;
+            }
+            if (node1.left != null) {
+                queue1.addLast(node1.left);
+            }
+            if (node1.right != null) {
+                queue1.addLast(node1.right);
+            }
+            if (node2.left != null) {
+                queue2.addLast(node2.left);
+            }
+            if (node2.right != null) {
+                queue2.addLast(node2.right);
+            }
+        }
+
+        return true;
+    }
+}
